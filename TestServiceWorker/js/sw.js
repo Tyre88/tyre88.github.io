780e64ac584f0daf328db7eb1ddea20386a7ec96
Tyre <victor@webbdudes.se>
2015-04-18 09:37:09 +0200

    Fixing with serviceworker...

--- TestServiceWorker/js/sw.js
+++ TestServiceWorker/js/sw.js
@@ -1,4 +1,8 @@
 self.addEventListener('install', function(event) {
+	cache.put('/TestServiceWorker/js/test.js', function(response)
+	{
+		console.log(response);
+	});
 	// pre cache a load of stuff:
 	event.waitUntil(
 		caches.open('testing').then(function(cache) {
