68a4a0764c21bacbb1db97f1e38ae8f045ca4b0e
victor <victor@webbdudes.se>
2015-07-09 15:26:27 +0200

    asd

--- TestPush2/notification-worker.js
+++ TestPush2/notification-worker.js
@@ -21,7 +21,7 @@ self.addEventListener('notificationclick', function(event) {
     event.notification.close();
 
     event.waitUntil(
-        clients.matchAll({'window'}).then(function(clientList) {
+        clients.matchAll({type: 'window'}).then(function(clientList) {
             for(var i = 0; i < clientList.length; i++)
             {
                 var client = clientList[i];
@@ -34,5 +34,5 @@ self.addEventListener('notificationclick', function(event) {
                 return clients.openWindow('/');
             }
         })
-    );
+    )
 });
