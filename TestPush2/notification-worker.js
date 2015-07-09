3df88c6291d0af25e887232ff094ac3058e84e43
victor <victor@webbdudes.se>
2015-07-09 15:43:11 +0200

    asd

--- TestPush2/notification-worker.js
+++ TestPush2/notification-worker.js
@@ -6,6 +6,15 @@
     var icon = '/images/icon-192x192.png';
     var tag = 'simple-push-demo-notification-tag';
 
+    if(event.data != null && event.data.title != null)
+        title = event.data.title;
+
+    if(event.data != null && event.data.message != null)
+        body = event.data.message;
+
+    if(event.data != null && event.data.icon != null)
+        icon = event.data.icon;
+
     event.waitUntil(
 		self.registration.showNotification(title, {
 		    body: body,
