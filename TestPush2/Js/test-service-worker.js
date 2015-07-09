ce6ba11887a5fb94a371be6831d6e9122b7e0ef8
victor <victor@webbdudes.se>
2015-07-09 15:00:31 +0200

    asd

---
+++ TestPush2/Js/test-service-worker.js
@@ -0,0 +1,59 @@
+﻿/* SERVICE WORKER TEST */
+
+function InitializeServiceWorker() {
+    if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+        console.warn('Notifications aint supported.');
+        return;
+    }
+
+    if (Notification.permission == 'denied') {
+        console.warn('The user have blocked the permissions');
+        return;
+    }
+
+    if (!('PushManager' in window)) {
+        console.warn('Push messages aint supported.');
+        return;
+    }
+
+    navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+        serviceWorkerRegistration.pushManager.getSubscription()
+            .then(function (subscription) {
+                if (!subscription) {
+                    console.warn('No subscription');
+                    return;
+                }
+            });
+    });
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('notification-worker.js').then(InitializeServiceWorker);
+}
+else {
+    console.warn('Service workers aint supported in the browser...');
+}
+
+function SubscribeToNotifications() {
+    navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+        serviceWorkerRegistration.pushManager.subscribe({ userVisibleOnly: true })
+            .then(function (subscription) {
+                debugger;
+                console.log('Subscribed!', subscription);
+                console.log(subscription.subscriptionId);
+                $("body").append(JSON.stringify(subscription));
+            })
+            .catch(function (err) {
+                if (Notification.permission == 'denied') {
+                    console.warn('The user have blocked the permissions')
+                }
+                else {
+                    console.warn('Unable to subscribe...', err);
+                }
+            });
+    });
+}
+
+$("#chkPushNotification").on('change', function () {
+    SubscribeToNotifications();
+}); 
---
+++ TestPush2/notification-worker.js
@@ -0,0 +1,16 @@
+﻿self.addEventListener('push', function (event) {
+    console.log('Received a push message', event);
+
+    var title = 'Test push notification...';
+    var body = 'Test message....';
+    var icon = '/images/icon-192x192.png';
+    var tag = 'simple-push-demo-notification-tag';
+
+    event.waitUntil(
+		self.registration.showNotification(title, {
+		    body: body,
+		    icon: icon,
+		    tag: tag
+		})
+	);
+});
