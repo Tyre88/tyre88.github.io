0be41b4107d17cab2904e3cf906b9c3c79026bac
victor <victor@webbdudes.se>
2016-03-08 08:46:20 +0100

    Test barcode.

--- barcode/app.js
+++ barcode/app.js
@@ -3,10 +3,10 @@ var arg = {
         $('body').append($('<li>' + result.format + ': ' + result.code + '</li>'));
     }
 };
-var decoder = $("canvas").WebCodeCamJQuery(arg).data().plugin_WebCodeCamJQuery.play();
+var decoder = $("canvas").WebCodeCamJQuery(arg).data().plugin_WebCodeCamJQuery;
 
 function decodeLocalImage(){
     decoder.decodeLocalImage();
 }
 
-decoder.buildSelectMenu('#camera-select', 'environment|back').init(arg);
+decoder.buildSelectMenu('#camera-select', 'environment|back').init(arg).play();
