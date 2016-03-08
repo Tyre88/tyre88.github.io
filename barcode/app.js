b560c660b1c958ab7cfe5370d86ef2540a84bd41
victor <victor@webbdudes.se>
2016-03-08 09:12:32 +0100

    Test barcode.

--- barcode/app.js
+++ barcode/app.js
@@ -1,24 +1,16 @@
+var txt = "innerText" in HTMLElement.prototype ? "innerText" : "textContent";
 var arg = {
-    decoderWorker: 'js/DecoderWorker.js',
     DecodeBarCodeRate: 5,
+    frameRate: 15,
+    tryVertical: true,
+    decoderWorker: 'DecoderWorker.js',
     resultFunction: function(result) {
-        $('body').append($('<li>' + result.format + ': ' + result.code + '</li>'));
+        var aChild = document.createElement('li');
+        aChild[txt] = result.format + ': ' + result.code;
+        document.querySelector('body').appendChild(aChild);
     }
 };
-var decoder = $("canvas").WebCodeCamJQuery(arg).data().plugin_WebCodeCamJQuery;
-decoder.buildSelectMenu("select");
-decoder.play();
-/*  Without visible select menu
- decoder.buildSelectMenu(document.createElement('select'), 'environment|back').init(arg).play();
- */
-
-
-
-$('select').on('change', function(){
-    decoder.stop().play();
-});
-
-$('#test').on('click', function() {
-    var url = decoder.getLastImageSrc();
-    decoder.decodeLocalImage(url);
-});
+var decoder = new WebCodeCamJS("canvas").init(arg);
+function decodeLocalImage(){
+    decoder.decodeLocalImage();
+}
